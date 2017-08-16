fdbdaadbaf085362ceb82aa12a1f4579730278af
Gagydzer <zhekanes@yandex.ru>
2017-08-16 16:20:07 +0300

    Add files via upload

--- quiz/js/quiz_code.js
+++ quiz/js/quiz_code.js
@@ -10,14 +10,10 @@ function quiz_nextslide(){
 	console.log('Текущий слайд', slidecounter );
 	$('.quiz_block_wrap').css('left', -600*(slidecounter));
 	console.log( 'Слайдер прокручен на', -600*(slidecounter) );
+	console.log('slidecounter, slidestotal',slidecounter, slidestotal);
 	quiz_progress();
 	}
-function quiz_previousslide(){
-	$('.quiz_block_wrap').css('left', 600*slidecounter);
-	console.log( 'Слайдер прокручен на', 600*slidecounter );
-	slidecounter--;
-	console.log( slidecounter );
-	}
+
 function quiz_progress(){
 	$('.quiz_progresscounter').css('width', (100*(slidecounter)/(slidestotal+1))+"%");
 	$('.quiz_progress').css('opacity', 0.7);
@@ -25,7 +21,8 @@ function quiz_progress(){
 function quiz_showresult(){
 	setTimeout( function() {$('.quiz_resultimg').css('-webkit-animation-name', 'scale-in');
 	$('.quiz_textcontent').css('-webkit-animation-name', 'from-left');
-	}, 4500);
+	}, 1);
+	console.log('показ результат');	
 }
 function quiz_show_media (right) {
 	
@@ -33,10 +30,6 @@ function quiz_show_media (right) {
 $('#nextslide, .btn').bind('click', function(){
 	$('.btn').addClass('animate');
 	quiz_nextslide();
-	console.log('sdelan click')
-	if (slidecounter == (slidestotal+1)) {
-	quiz_showresult()	
-	}
 	} );
 $('#previousslide').bind('click', function(){
 	quiz_previousslide();
@@ -51,10 +44,11 @@ function lalala() {
 $('.quiz_answers ul li#quiz_answer11, .quiz_answers ul li#quiz_answer12, .quiz_answers ul li#quiz_answer14').bind('click', function(){
 	$('.quiz_media_overlay, .quiz_title#wrong').css('display', 'block');
 	setTimeout( function(){ $('.quiz_media_overlay').addClass('display').addClass('wrong');
-		setTimeout( function(){ 
+	console.log('показ гифка');	
+		setTimeout( function(){ console.log('конец гифка');	
 			$('.quiz_media_overlay').removeClass('display')
-			}, 1500);
-	}, 200);
+			}, 1100);
+	}, 500);
 	setTimeout( function(){	
 		$('#quiz_answer11').addClass('wrong');
 		$('#quiz_answer12').addClass('wrong');
@@ -71,10 +65,14 @@ $('.quiz_answers ul li#quiz_answer11, .quiz_answers ul li#quiz_answer12, .quiz_a
 		$('.quiz_answers ul li .numbers').css('opacity', '1');
 		$('.quiz_answers ul li .numbers div').spincrement();
 	}, 1500);
-	setTimeout( function(){quiz_nextslide()}, 4000);
-	if (slidecounter == slidestotal) {
-	quiz_showresult()
-	}
+	console.log('slidecounter, slidestotal',slidecounter, slidestotal);
+	setTimeout( function(){quiz_nextslide()
+	if (slidecounter == (slidestotal+1)) {	
+	quiz_showresult();
+	}}, 4000);
+	
+	
+	
 	});
 
 $('.quiz_answers ul li#quiz_answer13').bind('click', function(){
@@ -82,7 +80,7 @@ $('.quiz_answers ul li#quiz_answer13').bind('click', function(){
 	setTimeout( function(){ $('.quiz_media_overlay').addClass('display').addClass('right');
 		setTimeout( function(){ 
 			$('.quiz_media_overlay').removeClass('display')
-			}, 1500);
+			}, 1100);
 	}, 500);
 	setTimeout( function(){	
 		$('#quiz_answer11').addClass('wrong');
@@ -100,8 +98,11 @@ $('.quiz_answers ul li#quiz_answer13').bind('click', function(){
 		$('.quiz_answers ul li .numbers').css('opacity', '1');
 		$('.quiz_answers ul li .numbers div').spincrement();
 	}, 3000);
-	setTimeout( function(){quiz_nextslide()}, 4000);
-	if (slidecounter == (slidestotal+1)) {
-	quiz_showresult()
-	}
+	setTimeout( function(){quiz_nextslide()
+	if (slidecounter == (slidestotal+1)) {	
+	quiz_showresult();
+	}}, 4000);
+	
+	
+	
 	});
